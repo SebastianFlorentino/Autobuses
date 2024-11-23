fa0dc10aa32a6604a22079da59205dea61ec24f0
Sebastian Florentino Brito <fsebastian432@outlook.es>
2024-11-22 21:47:30 -0400

    Agregando capa de negocio

---
+++ CapaNegocio/N_Rutas.cs
@@ -0,0 +1,44 @@
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class N_Rutas
+    {
+        D_rutas objDato = new D_rutas();
+
+        //Create
+
+        public void insertandoRutas(E_Rutas rutas)
+        {
+            objDato.InsertarRutas(rutas);
+
+        }
+
+        //Read
+
+        public List<E_Rutas> listandoRutas(string buscar)
+        {
+            return objDato.ListarRutas(buscar);
+        }
+
+        //Update
+
+        public void editandoRutas(E_Rutas rutas)
+        {
+            objDato.EditarRutas(rutas);
+        }
+
+        //Delete
+
+        public void eliminandoRutas(E_Rutas rutas)
+        {
+            objDato.EliminarRutas(rutas);
+        }
+    }
+}
