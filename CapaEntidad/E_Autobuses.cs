798178c3acbb44fe9ca1accaad99354389151995
Sebastian Florentino Brito <fsebastian432@outlook.es>
2024-11-22 21:46:32 -0400

    Agregando capa de entidad

---
+++ CapaEntidad/E_Autobuses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class E_Autobuses
+    {
+        public int IDAutobus { get; set; }
+        public string marca { get; set; }
+        public string modelo { get; set; }
+        public string placa { get; set; }
+        public string color { get; set; }
+        public string anno { get; set; }
+    }
+}
---
+++ CapaEntidad/E_Rutas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class E_Rutas
+    {
+        public int idRuta { get; set; }
+        public string nRuta { get; set; }
+    }
+}
